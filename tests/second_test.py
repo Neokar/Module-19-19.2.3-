307bb9ae4b2c83a6c618b22710e8fd15de2798bf
Neokar <wowoff95@yandex.ru>
2022-11-16 18:19:14 +0300

    final version

--- tests/second_test.py
+++ tests/second_test.py
@@ -9,7 +9,7 @@ class TestCalc:
     def test_multiply_calculate_correctly(self):
         assert self.calc.multiply(self, 2, 2) == 4
 
-    def test_division_calculate_correctly(self): \
+    def test_division_calculate_correctly(self):
             assert self.calc.division(self, 10, 5) == 2
 
     def test_subtraction_calculate_correctly(self):
